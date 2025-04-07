10ffbe54cfe58bf85db31438165ecc30905c8f6f
Dmitriy Derepko <xepozz@list.ru>
2025-04-07 12:15:14 +0300

    add release wf

--- build.gradle.kts
+++ build.gradle.kts
@@ -92,19 +92,6 @@ intellijPlatform {
             }
         }
 
-        val changelog = project.changelog // local variable for configuration cache compatibility
-        // Get the latest available change notes from the changelog file
-        changeNotes = providers.gradleProperty("pluginVersion").map { pluginVersion ->
-            with(changelog) {
-                renderItem(
-                    (getOrNull(pluginVersion) ?: getUnreleased())
-                        .withHeader(false)
-                        .withEmptySections(false),
-                    Changelog.OutputType.HTML,
-                )
-            }
-        }
-
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
             untilBuild = providers.gradleProperty("pluginUntilBuild")
@@ -132,12 +119,6 @@ intellijPlatform {
     }
 }
 
-// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
-changelog {
-    groups.empty()
-    repositoryUrl = providers.gradleProperty("pluginRepositoryUrl")
-}
-
 // Configure Gradle Kover Plugin - read more: https://github.com/Kotlin/kotlinx-kover#configuration
 kover {
     reports {
@@ -155,7 +136,6 @@ tasks {
     }
 
     publishPlugin {
-        dependsOn(patchChangelog)
     }
 
     // https://plugins.jetbrains.com/docs/marketplace/obfuscate-the-plugin.html#proguard
