d7c7f881ee8f3a72607fed1520cfb3d8052f902c
Dmitriy Derepko <xepozz@list.ru>
2025-10-01 20:04:02 +0400

    fix: optimize performance of line marker provider

--- src/main/kotlin/com/github/xepozz/gitattributes/ide/AttributesLineMarkerProvider.kt
+++ src/main/kotlin/com/github/xepozz/gitattributes/ide/AttributesLineMarkerProvider.kt
@@ -1,32 +1,25 @@
 package com.github.xepozz.gitattributes.ide
 
 import com.github.xepozz.gitattributes.language.psi.AttributesPattern
-import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.codeInsight.navigation.NavigationGutterIconBuilder
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileSystemItem
-import kotlin.collections.firstNotNullOfOrNull
 
 class AttributesLineMarkerProvider : RelatedItemLineMarkerProvider() {
-    override fun collectNavigationMarkers(
-        element: PsiElement,
-        result: MutableCollection<in RelatedItemLineMarkerInfo<*>>
-    ) {
-        when (element) {
-            is AttributesPattern -> {
-                val target = element.references.firstNotNullOfOrNull { it.resolve() } as? PsiFileSystemItem ?: return
-//                println("target: $target")
+    override fun getLineMarkerInfo(element: PsiElement) = when {
+        element is AttributesPattern -> {
+            val target = element.references.firstNotNullOfOrNull { it.resolve() } as? PsiFileSystemItem ?: return null
+//            println("target: $target")
 
-                val builder = NavigationGutterIconBuilder.create(target.getIcon(0) ?: AllIcons.Nodes.Target)
-                    .setTargets(target)
-                    .setTooltipText("Navigate to ${target.name}")
+            val builder = NavigationGutterIconBuilder.create(target.getIcon(0) ?: AllIcons.Nodes.Target)
+                .setTargets(target)
+                .setTooltipText("Navigate to ${target.name}")
 
-                result.add(builder.createLineMarkerInfo(element))
-            }
+            builder.createLineMarkerInfo(element.firstChild)
         }
+
+        else -> null
     }
 }
